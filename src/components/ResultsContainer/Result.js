348da9e718fe8d52c9415410df5bdb3ff937d8e4
cquinones100 <cquinones100@gmail.com>
2018-03-01 16:36:36 -0500

    style changes

--- src/components/ResultsContainer/Result.js
+++ src/components/ResultsContainer/Result.js
@@ -2,13 +2,10 @@ import React from 'react'
 import './Result.css'
 
 const Result = ({ name, onClick, category }) =>
-  <div
-    className='result'
-    id={name}
-    onClick={onClick}
-  >
-    <strong>{category}: </strong>{name}
-
+  <div className='result' id={name} onClick={onClick}>
+    <div onClick={onClick} id={name} className='result-inner-text'>
+      <strong onClick={onClick} id={name}>{category}: </strong>{name}
+    </div>
   </div>
 
 export default Result
